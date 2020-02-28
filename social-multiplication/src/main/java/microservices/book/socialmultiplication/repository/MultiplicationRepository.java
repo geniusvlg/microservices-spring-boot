c39e01faf8712f101f7ae0e1e95c69aac2dd97c5
huyhpnguyen <huylvl1994@gmail.com>
2020-02-28 15:52:47 +0700

    Refactor

--- social-multiplication/src/main/java/microservices/book/socialmultiplication/repository/MultiplicationRepository.java
+++ social-multiplication/src/main/java/microservices/book/socialmultiplication/repository/MultiplicationRepository.java
@@ -1,9 +1,10 @@
 package microservices.book.socialmultiplication.repository;
 
+import microservices.book.socialmultiplication.domain.Multiplication;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * This interface allows us to save and retrieve Multiplications
  */
-public interface MultiplicationRepository extends CrudRepository<MultiplicationRepository,Long> {
+public interface MultiplicationRepository extends CrudRepository<Multiplication, Long> {
 }
--- social-multiplication/src/main/resources/static/multiplication-client.js
+++ social-multiplication/src/main/resources/static/multiplication-client.js
@@ -19,10 +19,13 @@ function updateStats(alias) {
     $.ajax({
         url: "http://localhost:8080/results?alias=" + alias,
     }).then(function(data) {
+        $('#stats-body').empty();
+        data.forEach(function( row ) {
         $('#stats-body').append('<tr><td>' + row.id + '</td><td>' + row.multiplication.factorA + ' x '
-        + row.multiplication.factorB + '</td><td>' + row.resultAttempt + '</td><td>'
-        + (row.correct === true ? 'YES' : 'NO') + '</td></tr>');
-    })
+                + row.multiplication.factorB + '</td><td>' + row.resultAttempt + '</td><td>'
+                + (row.correct === true ? 'YES' : 'NO') + '</td></tr>');
+        });
+    });
 
 }
 
@@ -38,7 +41,7 @@ $(document).ready(function() {
         var b = $('.multiplication-b').text();
         var $form = $(this),
             attempt = $form.find("input[name='result-attempt']").val(),
-            userAlias = $form.find("input[name=''user-alias']").val();
+            userAlias = $form.find("input[name='user-alias']").val();
 
         // Compose the data in the format that the API is expecting
         var data = { user: { alias: userAlias},
